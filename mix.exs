f07679c8e87ea11b4c1fb98e0f8378f9ac0f5416
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2014-10-06 13:08:58 +0200

    Tweak mix.exs

--- mix.exs
+++ mix.exs
@@ -2,32 +2,35 @@ defmodule TrailingFormatPlug.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :trailing_format_plug,
-     version: "0.0.1",
-     elixir: "~> 1.0.0",
-     deps: deps]
+    [
+      app: :trailing_format_plug,
+      version: "0.0.1",
+      elixir: "~> 1.0.0",
+      deps: deps,
+      package: [
+        contributors: "Michael Schaefermeyer",
+        licenses: ["MIT"],
+        links: [github: "http://github.com/mschae/trailing_format_plug"]
+      ],
+      description: description
+     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :cowboy]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:cowboy, "~> 1.0.0"},
       {:plug, "~> 0.8.1"}
     ]
+  end
+
+  defp description do
+    """
+    An elixir plug to support legacy APIs that use a rails-like trailing
+    format: http://api.dev/resources.format
+    """
   end
 end
