0e8f2c65fb9598c7e831ee1777175dae2c1b522c
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2017-01-13 15:02:03 +0100

    Refactor

--- lib/trailing_format_plug.ex
+++ lib/trailing_format_plug.ex
@@ -5,25 +5,38 @@ defmodule TrailingFormatPlug do
 
   def call(%{path_info: []} = conn, _opts), do: conn
   def call(conn, _opts) do
-    conn.path_info |> List.last |> String.split(".") |> Enum.reverse |> case do
-      [ _ ] -> conn
+    path = conn.path_info |> List.last |> String.split(".") |> Enum.reverse
+
+    case path do
+      [ _ ] ->
+        conn
+
       [ format | fragments ] ->
         new_path       = fragments |> Enum.reverse |> Enum.join(".")
         path_fragments = List.replace_at conn.path_info, -1, new_path
         params         =
           Plug.Conn.fetch_query_params(conn).params
           |> update_params(new_path, format)
-          |> Dict.put("_format", format)
-        %{conn | path_info: path_fragments, query_params: params, params: params}
+          |> Map.put("_format", format)
+
+        %{
+          conn |
+          path_info: path_fragments,
+          query_params: params,
+          params: params
+        }
     end
   end
 
   defp update_params(params, new_path, format) do
-    params
-    |> Enum.find(fn {key, val} -> val == "#{new_path}.#{format}" end)
-    |> case do
-      {key, _} -> Map.put(params, key, new_path)
-      _ -> params
+    wildcard = Enum.find params, fn {_, v} -> v == "#{new_path}.#{format}" end
+
+    case wildcard do
+      {key, _} ->
+        Map.put(params, key, new_path)
+
+      _ ->
+        params
     end
   end
 end
